cd31b66c982490e16c29018d18e84ae9bc0b76a8
Jeremy Wells <jemmyw@gmail.com>
2017-01-27 22:51:24 +1300

    Remove some files

--- schemas/json-schema-to-typescript/dist/src/generator.d.ts
+++
@@ -1,3 +0,0 @@
-import { Options } from './index';
-import { AST } from './parser';
-export declare function generate(ast: AST, options?: Options): string;
--- schemas/json-schema-to-typescript/dist/src/refResolver.d.ts
+++
@@ -1,9 +0,0 @@
-import { JSONSchema } from './JSONSchema';
-/**
- * Crawls a JSONSchema's $refs, and returns a list of all
- * referenced schemas
- */
-export declare function collectRefs(schema: JSONSchema): {
-    [schemaId: string]: JSONSchema;
-};
-export declare function readSchema(filename: string): JSONSchema;
